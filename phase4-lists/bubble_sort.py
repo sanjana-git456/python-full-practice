3279f723cd80e882618a4e445c8bcd46227bb657
Sanjana Khajuria <sanjanakhajuria10@gmal.com>
2026-05-29 18:31:18 +0530

    bubble sort

---
+++ phase4-lists/bubble_sort.py
@@ -0,0 +1,9 @@
+x = list(map(int, input("Enter numbers: ").split()))
+l = len(x)
+for i in range(l):
+    for j in range(l-i-1):
+        if x[j] > x[j+1]:
+            temp = x[j]
+            x[j] = x[j+1]
+            x[j+1] = temp
+print(f"Sorted: {x}")
