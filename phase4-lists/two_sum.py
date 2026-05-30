87be5b4c41d925987796638c1c4646c16341b0bb
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-05-31 01:40:23 +0530

    two sum

---
+++ phase4-lists/two_sum.py
@@ -0,0 +1,21 @@
+x = list(map(int, input("Enter numbers: ").split()))
+t = int(input("Enter target: "))
+found = False
+for i in range(len(x)):
+    for j in range(i+1,len(x)):
+        if (x[i] + x[j]) == t:
+            print(f"Numbers at index {i} and {j} add up to {t}")
+            found = True
+            break
+    if found:
+        break
+if not found:
+    print("Cannot happen")
+
+d = {}
+for i in range(len(x)):
+    compliment = t-x[i]
+    if compliment in d:
+        print(f"Numbers at index {d[compliment]} and {i} add up to {t}")
+        break
+    d[x[i]] = i
